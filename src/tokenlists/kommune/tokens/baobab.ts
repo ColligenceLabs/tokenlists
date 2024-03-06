2730eb6af6b7a4cd4a4c7ad31d2f0f533db44f1e
Peter H. Nahm <hwnahm@gmail.com>
2024-03-06 12:11:38 +0900

    chore: newly deployed baobab tokens

--- src/tokenlists/kommune/tokens/baobab.ts
+++ src/tokenlists/kommune/tokens/baobab.ts
@@ -1,6 +1,7 @@
 export default [
   '0x0339d5eb6d195ba90b13ed1bceaa97ebd198b106', // WKLAY
-  '0xe611ab92bad2797c849844e07ca61c615617a788', // USDC (Talken)
-  '0xaca0dcb8b6ca87889db61c66ec08b43a5197c0cc', // USDT (Talken)
-  '0xe6d28e683481dc29f32624b410825c7273d45e1d', // DAI (Talken)
+  '0xe48bbabc85eb49c629226e421ba54c793fa5d19e', // USDC (Talken)
+  '0x985acd34f36d91768ad4b0cb295aa919a7abdb27', // USDT (Talken)
+  '0x4828ea3e78b04504886c8d0f45e3abb3c52499b1', // DAI (Talken)
+  '0x667dcb81dea11870e7a52c9d1e9e9f2760ce966e', // BAL - TALK
 ]
