a0b0463c5e9225bc2bbbd8839dd5fd58ff7a9f45
Peter H. Nahm <hwnahm@gmail.com>
2024-03-18 14:20:11 +0900

    chore: deploy KDO

--- src/tokenlists/kommune/tokens/baobab.ts
+++ src/tokenlists/kommune/tokens/baobab.ts
@@ -3,5 +3,5 @@ export default [
   '0xe48bbabc85eb49c629226e421ba54c793fa5d19e', // USDC (Talken)
   '0x985acd34f36d91768ad4b0cb295aa919a7abdb27', // USDT (Talken)
   '0x4828ea3e78b04504886c8d0f45e3abb3c52499b1', // DAI (Talken)
-  '0x667dcb81dea11870e7a52c9d1e9e9f2760ce966e', // BAL - TALK
+  '0xade511125cb633f190160b2876ca3bcec93dcca7', // KDO
 ]
