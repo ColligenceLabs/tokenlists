10adea4dea7bd580dbdaf628b059d846bc0531c6
Peter H. Nahm <hwnahm@gmail.com>
2024-03-26 09:43:31 +0900

    chore: all TALK

--- src/tokenlists/kommune/tokens/baobab.ts
+++ src/tokenlists/kommune/tokens/baobab.ts
@@ -4,4 +4,5 @@ export default [
   '0x985acd34f36d91768ad4b0cb295aa919a7abdb27', // USDT (Talken)
   '0x4828ea3e78b04504886c8d0f45e3abb3c52499b1', // DAI (Talken)
   '0xade511125cb633f190160b2876ca3bcec93dcca7', // KDO
+  '0x667dcb81dea11870e7a52c9d1e9e9f2760ce966e', // Talk
 ]
