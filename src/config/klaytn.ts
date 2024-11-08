edbdbf53ccd5a27f66a4e02ed2c2bb61ccc86b4e
Peter H. Nahm <hwnahm@gmail.com>
2024-11-08 11:25:17 +0900

    chore: change symbols

--- src/config/klaytn.ts
+++ src/config/klaytn.ts
@@ -7,5 +7,5 @@ export default {
   addresses: {
     multicaller: '0xa113b74a396932A18e01AD1E2C388B515e7d59e0',
   },
-  rpc: 'https://klaytn-en.kommunedao.xyz:8651',
+  rpc: 'https://public-en.node.kaia.io',
 }
