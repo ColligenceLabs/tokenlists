0c8dee0da5d67a2f11a29bea8335760d2947c219
Peter H. Nahm <hwnahm@gmail.com>
2025-07-29 08:46:49 +0900

    chore: add more token

--- src/config/baobab.ts
+++ src/config/baobab.ts
@@ -7,5 +7,5 @@ export default {
   addresses: {
     multicaller: '0xa8e1e54428D8C761291E5F2877A24c4173fF2BB4',
   },
-  rpc: 'https://klaytn-baobab.blockpi.network/v1/rpc/public',
+  rpc: 'https://public-en-kairos.node.kaia.io',
 }
